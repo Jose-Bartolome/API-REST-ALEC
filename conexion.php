8801328943507d9d035b9bb9a8f200956b40b9ec
Jose-Bartolome <92230571+Jose-Bartolome@users.noreply.github.com>
2021-11-15 19:36:13 -0600

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,10 +1,10 @@
 <?php
 class Conexion extends PDO
 {
-    public $hostdb = 'localhost';
-    public $bdname = 'alec';
-    public $usuario = 'root';
-    public $password = '';
+    public $hostdb = 'bvmsi8xlfpo4eoqm8huh-mysql.services.clever-cloud.com';
+    public $bdname = 'bvmsi8xlfpo4eoqm8huh';
+    public $usuario = 'ubbdnduwftfi4ixe';
+    public $password = 'byKV4JdznwI3aAe3so57';
 
     public function __construct()
     {
